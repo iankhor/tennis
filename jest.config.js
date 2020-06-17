f2e34ab04b590da925f97594b7a0aa430ff3aa73
Ian Khor <iankhorbc.thecoder@gmail.com>
2020-06-18 08:47:45 +1000

    add inquirer-test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
 	preset: 'ts-jest',
-	setupFilesAfterEnv: ['<rootDir>/testlib/test-setup.js'],
 	moduleNameMapper: {
 		'^components/([^\\.]*)$': '<rootDir>/src/components/$1',
 		'^testlib/([^\\.]*)$': '<rootDir>/testlib/$1',
